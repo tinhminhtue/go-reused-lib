1dbfc06f74ed1af7b4fc1ebda7bd62d4f18e8928
Kha <khanguyenmail@gmail.com>
2025-04-17 14:27:54 +0700

    ^ just commit last stage to master

--- core/nats/jwtnats/jwtnats.go
+++ core/nats/jwtnats/jwtnats.go
@@ -6,19 +6,18 @@ import (
 
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/nkeys"
-	"github.com/spf13/viper"
 )
 
 func GetAccountPublicKey() string {
-	return viper.GetString("nats.account.public_key")
-	// return "AADJYH5MPADBUXFF6NROB3VDMANPTGBOMZJ474VYHFO4N7UANDHDZQ5P"
+	// return viper.GetString("nats.account.public_key")
+	return "AADJYH5MPADBUXFF6NROB3VDMANPTGBOMZJ474VYHFO4N7UANDHDZQ5P"
 }
 
-func GetAccountSigningKey() nkeys.KeyPair {
+func GetAccountSigningKey(accountSigningSeedKey string) nkeys.KeyPair {
 	// Content of the account signing key seed can come from a file or an environment variable as well
 
-	accSeed := []byte(viper.GetString("nats.account.signing_key"))
-	// "SAAPZPXHT354ODAL7LSFFVXW4KNZTAAFDU6G43I2P4QF6FH7X5KDH47K44"
+	accSeed := []byte(accountSigningSeedKey)
+	// accSeed := []byte("SAAPZPXHT354ODAL7LSFFVXW4KNZTAAFDU6G43I2P4QF6FH7X5KDH47K44")
 	accountSigningKey, err := nkeys.ParseDecoratedNKey(accSeed)
 	if err != nil {
 		panic(err)
@@ -34,7 +33,7 @@ func GetAccountSigningKey() nkeys.KeyPair {
 func ConnectUserDemo() {
 	// Setup! Obtain the account signing key!
 	accountPublicKey := GetAccountPublicKey()
-	accountSigningKey := GetAccountSigningKey()
+	accountSigningKey := GetAccountSigningKey("SAAPZPXHT354ODAL7LSFFVXW4KNZTAAFDU6G43I2P4QF6FH7X5KDH47K44")
 	// userPublicKey, userSeed, userKeyPair := generateUserKey() // userSeed is not used in this example
 	userPublicKey, userNkeySeed, userKeyPair := generateUserKey() // userSeed is not used in this example
 	// userPublicKey, _, userKeyPair := generateUserKey()
@@ -42,8 +41,8 @@ func ConnectUserDemo() {
 	// print userNkeySeed
 	log.Println("userNkeySeed: ", string(userNkeySeed))
 	userClaim := UserClaim{
-		PubAllows: []string{"subject.bar"}, // only allow publishing to subject.bar
-		SubAllows: []string{"subject.foo"}, // only allow subscribing to subject.foo
+		PubAllows: []string{"subject.foo"}, // only allow publishing to subject.foo
+		SubAllows: []string{"subject.bar"}, // only allow subscribing to subject.bar
 		Expires:   time.Now().Add(time.Hour).Unix(),
 	}
 	userJWT, err := generateUserJWT(userClaim, userPublicKey, accountPublicKey, accountSigningKey)
@@ -51,19 +50,25 @@ func ConnectUserDemo() {
 		panic(err)
 	}
 	// connect with System NKey to update user claims
-	adminJwt := viper.GetString("nats.admin.jwt")
-	adminSeed := viper.GetString("nats.admin.seed")
-	natsAdminOption := nats.UserJWTAndSeed(adminJwt, adminSeed)
-	ncSys, err := nats.Connect("127.0.0.1", natsAdminOption)
+	// adminJwt := viper.GetString("nats.admin.jwt")
+	// adminSeed := viper.GetString("nats.admin.seed")
+	// natsAdminOption := nats.UserJWTAndSeed(adminJwt, adminSeed)
+	// ncSys, err := nats.Connect("127.0.0.1", natsAdminOption)
 
-	// ncSys, err := nats.Connect("127.0.0.1", nats.UserCredentials("admin.creds"))
+	// This code wont work: jwt update resulted in error - n/a - not account claim (only account claim is needed)
+	ncSys, err := nats.Connect("127.0.0.1", nats.UserCredentials("admin.creds"))
 	if err != nil {
 		panic(err)
 	}
-	ncSys.Request("$SYS.REQ.CLAIMS.UPDATE", []byte(userJWT), time.Second)
+	msg, err := ncSys.Request("$SYS.REQ.CLAIMS.UPDATE", []byte(userJWT), time.Second)
+	if err != nil {
+		if err.Error() != "nats: no responders available for request" {
+			panic(err)
+		}
+	}
+	log.Println("Updated user claims: ", string(msg.Data))
 
 	// userJWT and userKeyPair can be used in conjunction with this nats.Option
-	// var jwtAuthOption nats.Option
 	jwtAuthOption := nats.UserJWT(func() (string, error) {
 		return userJWT, nil
 	},
@@ -73,7 +78,7 @@ func ConnectUserDemo() {
 	)
 
 	// Alternatively you can create a creds file and use it as nats.Option
-	// credsContent, err := jwt.FormatUserConfig(userJWT, userSeed)
+	// credsContent, err := jwt.FormatUserConfig(userJWT, userNkeySeed)
 	// if err != nil {
 	// 	panic(err)
 	// }
--- core/nats/jwtnats/type.go
+++ core/nats/jwtnats/type.go
@@ -9,12 +9,13 @@ import (
 )
 
 type NatsJwtConfig struct {
-	ServerUrl             string
-	AdminJwt              string // jwt is public key and claims
-	AdminSeed             string // seed is private key
-	AccountPublicKey      string // as ID for the account
-	AccountSigningSeedKey nkeys.KeyPair
-	TimeoutForJwtUpdate   time.Duration
+	ServerUrl                 string
+	AdminJwt                  string // jwt is public key and claims
+	AdminSeed                 string // seed is private key
+	AccountPublicKey          string // as ID for the account
+	AccountSigningSeedKey     string
+	accountSigningSeedKeyPair nkeys.KeyPair
+	TimeoutForJwtUpdate       time.Duration
 }
 
 type NatsJwtProvider struct {
@@ -42,6 +43,8 @@ var Instance = NatsJwtProvider{}
 // Setup the global instance, call this fuction before using the global instance
 func Setup(config NatsJwtConfig) {
 	Instance.Config = config
+	// update the account signing key
+	Instance.Config.accountSigningSeedKeyPair = GetAccountSigningKey(Instance.Config.AccountSigningSeedKey)
 }
 
 func cachedConnection() (*nats.Conn, error) {
@@ -50,6 +53,7 @@ func cachedConnection() (*nats.Conn, error) {
 	if Instance.AdminConn == nil || Instance.AdminConn.IsClosed() {
 		natsAdminOption := nats.UserJWTAndSeed(Instance.Config.AdminJwt, Instance.Config.AdminSeed)
 		ncSys, err := nats.Connect(Instance.Config.ServerUrl, natsAdminOption)
+
 		if err != nil {
 			return nil, err
 		}
@@ -62,22 +66,70 @@ func cachedConnection() (*nats.Conn, error) {
 
 func RequestUser(userClaim UserClaim) (userKeys UserJwtKeyPair, err error) {
 	userPublicKey, userSeed, _ := generateUserKey()
-	userJWT, err := generateUserJWT(userClaim, userPublicKey, Instance.Config.AccountPublicKey, Instance.Config.AccountSigningSeedKey)
+	userJWT, err := generateUserJWT(userClaim, userPublicKey, Instance.Config.AccountPublicKey, Instance.Config.accountSigningSeedKeyPair)
 	if err != nil {
 		return userKeys, err
 	}
 	userKeys.JwtKey = userJWT
 	userKeys.SeedKey = string(userSeed)
-	// connect with System NKey to update user claims
-	ncSys, err := cachedConnection()
-	if err != nil {
-		return userKeys, err
-	}
-	msg, err := ncSys.Request("$SYS.REQ.CLAIMS.UPDATE", []byte(userJWT), Instance.Config.TimeoutForJwtUpdate)
-	if err != nil {
-		return userKeys, err
-	}
-	logrus.Infof("Nats Jwt Claims Response: %s", string(msg.Data))
+
+	// connect with System NKey to update user claims (This is no need, only required for Account level)
+
+	// ncSys, err := cachedConnection()
+	// if err != nil {
+	// 	return userKeys, err
+	// }
+	// _, err = ncSys.Request("$SYS.REQ.CLAIMS.UPDATE", []byte(userJWT), Instance.Config.TimeoutForJwtUpdate)
+	// if err != nil {
+	// 	if err.Error() == "nats: no responders available for request" { // false positive error, nats server works fine for the update
+	// 		err = nil
+	// 	} else {
+	// 		return userKeys, err
+	// 	}
+	// }
 
 	return userKeys, nil
+}
+
+// this function is used to test connect to NATS server with the user keys
+func TestUserKeys(userKeys UserJwtKeyPair) (err error) {
+	// userJWT and userKeyPair can be used in conjunction with this nats.Option
+	userKeyPair, err := nkeys.FromSeed([]byte(userKeys.SeedKey))
+	if err != nil {
+		return err
+	}
+	jwtAuthOption := nats.UserJWT(func() (string, error) {
+		return userKeys.JwtKey, nil
+	},
+		func(bytes []byte) ([]byte, error) {
+			return userKeyPair.Sign(bytes)
+		},
+	)
+
+	// use in a connection as desired
+	nc, err := nats.Connect(Instance.Config.ServerUrl, jwtAuthOption)
+	if err != nil {
+		panic(err)
+	}
+	if nc.IsConnected() {
+		defer nc.Close()
+		logrus.Println("Connected to NATS")
+	}
+
+	// test subscribe subject.foo
+	_, err = nc.Subscribe("subject.foo", func(msg *nats.Msg) {
+		logrus.Println("Received message: ", string(msg.Data))
+	})
+	if err != nil {
+		return err
+	}
+
+	// try to publish subject.foo
+	err = nc.Publish("subject.foo", []byte("Hello World!"))
+	if err != nil { // NOTE: This will fail because the user is not allowed to publish to subject.foo (but server will not return an error)
+		logrus.Println("Failed to publish message")
+	}
+	// wait for the message to be received
+	time.Sleep(5 * time.Second)
+	return nil
 }
